db49e557be87761388bd847cdb063d40bf5d66fa
Bidhya Adhikari <141386969+bidhyaAdh@users.noreply.github.com>
2024-05-08 21:14:15 +0545

    Add files via upload

---
+++ Relational_operators.java
@@ -0,0 +1,17 @@
+class Relational_operators
+{
+    public static void main(String[] args)
+    {
+        //we use (==) to compare exactly equal the value
+        //we use = to assign th value
+        // we use (!=) for not equal
+
+        int x = 7;
+        int y = 5;
+ 
+        boolean result = x <= y ;  // compare the value
+        System.out.println(result);
+
+
+    }
+}    
